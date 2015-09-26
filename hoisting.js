15874ce543d1ac7abc3031b911dfeba38585a705
Caio Cutrim <kaiocoutrim@gmail.com>
2015-09-25 22:13:07 -0300

    initial commit

---
+++ hoisting.js
@@ -0,0 +1,21 @@
+// 
+// this, arguments
+// functions arguments
+// function declarations
+// variable declarations
+//
+//
+//
+
+var name = "Paulo";
+
+function sayName (name){
+
+	console.log(name);
+	var name = "Caio";
+	console.log(name);
+}
+
+
+
+sayName("Paulo");//call
